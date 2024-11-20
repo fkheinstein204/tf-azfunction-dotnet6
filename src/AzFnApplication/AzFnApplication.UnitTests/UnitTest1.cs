54fccb3d7eb30d389515da7fd57d67649d198125
fkheinstein <fkheinstein@gmail.com>
2024-11-20 12:28:22 +0100

    add xunit test project for UnitTest

---
+++ src/AzFnApplication/AzFnApplication.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace AzFnApplication.UnitTests
+{
+    public class UnitTest1
+    {
+        [Trait("Category", "Unit")]
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true); // NEVER DO THIS!  (Only for DEMO)
+        }
+    }
+}

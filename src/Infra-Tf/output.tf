1b43d193b90147a6917f762aa6ea3abcf38a4d62
heinstein <3276604+heinsteinh@users.noreply.github.com>
2024-12-03 12:40:26 +0100

    Update output

--- src/Infra-Tf/output.tf
+++ src/Infra-Tf/output.tf
@@ -5,5 +5,5 @@ output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
 output "function_name" {
-  value = azurerm_linux_function_app.foo.name
+  value = azurerm_linux_function_app.fn_linux_app.name
 }

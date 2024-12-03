a972a4c3430ca12b2b516baf7d721a99b31bb649
Heinstein F. <heinsteinh@gmail.com>
2024-12-03 21:33:51 +0100

    Terraform plan add to CD pipeline

--- src/dotnet/AzFnApplication/AzFnApplication/AzFnApplication..cs
+++ src/dotnet/AzFnApplication/AzFnApplication/AzFnApplication..cs
@@ -26,7 +26,7 @@ namespace AzFnApplication
 
         [FunctionName("Function1")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "func")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");

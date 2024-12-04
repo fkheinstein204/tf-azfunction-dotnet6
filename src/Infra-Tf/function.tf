5cc87d9d01255c7ad7a2a601005608eb34480eef
Heinstein F. <heinsteinh@gmail.com>
2024-12-04 14:29:55 +0100

    FUNCTIONS_WORKER_RUNTIME set to dotnet

--- src/Infra-Tf/function.tf
+++ src/Infra-Tf/function.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_function_app" "fn_linux_app" {
   }
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 1,
-    "FUNCTIONS_WORKER_RUNTIME"  =  "dotnet-isolated"
+    "FUNCTIONS_WORKER_RUNTIME"  =  "dotnet"
   }
 
   identity {

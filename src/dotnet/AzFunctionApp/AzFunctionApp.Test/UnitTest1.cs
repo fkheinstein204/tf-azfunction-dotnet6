1352374b0f6f390e38de70e2f3aeb43993dd1ae5
Heinstein F. <heinsteinh@gmail.com>
2024-12-04 09:40:25 +0100

    Add New Function App

---
+++ src/dotnet/AzFunctionApp/AzFunctionApp.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace AzFunctionApp.Test
+{
+    public class UnitTest1
+    {
+        [Trait("Category", "Unit")]
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true); //For pipeline testing purpose
+        }
+    }
+}
